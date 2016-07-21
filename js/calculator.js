7b3cc216c8933e66184c1f5ff4e0918e1805b212
Adriana Molina <moladriana@gmail.com>
2016-07-21 10:14:03 +0200

    New function that adds numbers to display using just a class

--- js/calculator.js
+++ js/calculator.js
@@ -5,75 +5,7 @@ $(document).ready(function() {
   var calculation = 0;
 
   $(".btn-push").click(function(){
-    alert($(this).html());
-  })
-
-  $("#btn-one").on("click", function() {
-    number = number + "1";
-    formula = formula + number;
-    console.log(number);
-    $("#numberScreen").html(number);
-    $("#operation").html(formula);
-  })
-
-  $("#btn-two").on("click", function() {
-    number = number + "2";
-    console.log(number);
-    $("#numberScreen").html(number);
-    $("#operation").html(formula);
-  })
-
-  $("#btn-three").on("click", function() {
-    number = number + "3";
-    console.log(number);
-    $("#numberScreen").html(number);
-    $("#operation").html(formula);
-  })
-
-  $("#btn-four").on("click", function() {
-    number = number + "4";
-    console.log(number);
-    $("#numberScreen").html(number);
-    $("#operation").html(formula);
-  })
-
-  $("#btn-five").on("click", function() {
-    number = number + "5";
-    console.log(number);
-    $("#numberScreen").html(number);
-    $("#operation").html(formula);
-  })
-
-  $("#btn-six").on("click", function() {
-    number = number + "6";
-    console.log(number);
-    $("#numberScreen").html(number);
-    $("#operation").html(formula);
-  })
-
-  $("#btn-seven").on("click", function(){
-    number = number + "7";
-    console.log(number);
-    $("#numberScreen").html(number);
-    $("#operation").html(formula);
-  })
-
-  $("#btn-eight").on("click", function(){
-    number = number + "8";
-    console.log(number);
-    $("#numberScreen").html(number);
-    $("#operation").html(formula);
-  })
-
-  $("#btn-nine").on("click", function(){
-    number = number + "9";
-    console.log(number);
-    $("#numberScreen").html(number);
-    $("#operation").html(formula);
-  })
-
-  $("#btn-zero").on("click", function(){
-    number = number + "0";
+    number = number + $(this).attr("value");
     console.log(number);
     $("#numberScreen").html(number);
     $("#operation").html(formula);
